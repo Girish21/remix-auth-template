f6e5f4d73363d9e4f02317f281784a1d31ca9fce
girish21 <girishkumarv21@gmail.com>
2021-08-22 14:36:08 +0530

    prisma enhancements

--- app/utils/prisma.server.ts
+++ app/utils/prisma.server.ts
@@ -1,10 +1,36 @@
 import prisma from '../prisma'
+import { generateHash } from './bcrypt.server'
 import { sessionExpirationTime } from './session.server'
+
+const geExpirationDate = () => new Date(Date.now() + sessionExpirationTime)
 
 const getUserFromSessionId = async (id: string) => {
   return prisma.session.findUnique({
     where: { id },
     select: { user: true },
+  })
+}
+
+const createUserIfNotExist = async (
+  email: string,
+  password: string,
+  name: string
+) => {
+  const userExist = await prisma.user.findUnique({ where: { email } })
+
+  if (userExist) {
+    throw new Error('accout with this email already exist, please login')
+  }
+
+  const hashPassword = await generateHash(password)
+
+  return prisma.user.create({
+    data: {
+      email,
+      password: hashPassword,
+      firstName: name,
+    },
+    select: { id: true },
   })
 }
 
@@ -19,7 +45,7 @@ const createSession = async (id: string) => {
   return prisma.session.create({
     data: {
       userId: id,
-      expirationDate: new Date(Date.now() + sessionExpirationTime),
+      expirationDate: geExpirationDate(),
     },
   })
 }
@@ -28,4 +54,10 @@ const deleteSession = async (id: string) => {
   return prisma.session.delete({ where: { id } })
 }
 
-export { createSession, deleteSession, getUserFromEmail, getUserFromSessionId }
+export {
+  createSession,
+  createUserIfNotExist,
+  deleteSession,
+  getUserFromEmail,
+  getUserFromSessionId,
+}
